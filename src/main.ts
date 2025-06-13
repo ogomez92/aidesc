9d952f04a53e8ba78f969be249c0f5e24733c707
Oriol Gómez <ogomez.s92@gmail.com>
2025-06-13 10:20:05 +0200

    feat: add i18n

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
+import englishTranslation from '@assets/locale/en.json';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
 import './style.css';
+const i18n = createI18n({
+  locale: 'en', // Set default locale
+  messages: {
+    en: englishTranslation,
+  },
+});
 
 const app = createApp(App);
 const pinia = createPinia();
-
+app.use(i18n);
 app.use(pinia)
     .mount('#app')
     .$nextTick(() => {
